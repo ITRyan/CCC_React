cc6a4bd20a508d01bc55c931414b97e26af769a1
unknown <itryanchan@gmail.com>
2023-05-12 12:45:43 -0700

    fix buge

---
+++ ryan.js
@@ -0,0 +1,8 @@
+console.log("Hello world")
+import React from 'react'
+
+export const ryan  = () => {
+  return (
+    <div>ryan  hello</div>
+ )
+}
---
+++ try.js
@@ -0,0 +1,13 @@
+const Array1 =[1,2,3,4,5,6,7,8,9]
+
+
+const Ryan = () =>{
+for (let i=0;i<Array1.length;i++){
+
+ console.log(i)   
+}
+
+return
+}
+console.log ("Hello World")
+Ryan()
