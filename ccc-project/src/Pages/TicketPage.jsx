f8813b629a239b9c881eb1e1e610ed66978982f1
Samson <samsonhlh@gmail.com>
2023-05-28 21:28:34 +0800

    ticketpage

--- ccc-project/src/Pages/TicketPage.jsx
+++ ccc-project/src/Pages/TicketPage.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
 import Sidebar from "../Components/Sidebar";
-const TicketPage = () => {
-  return (
-    <Sidebar/>
-  )
-}
 
-export default TicketPage
+const TicketPage = () => {
+  const handleReturn = () => {
+    // Handle return button functionality
+    console.log('Return button clicked!');
+  };
+
+  return (
+    <div>
+      <h1>Upcoming Booking</h1>
+      <div className="ticketing-form">
+        <div className="form-row">
+          <label htmlFor="movie">Movie:</label>
+          <select id="movie">
+          </select>
+        </div>
+        <div className="form-row">
+          <label htmlFor="date">Date:</label>
+          <input type="date" id="date" />
+        </div>
+        <div className="form-row">
+          <label htmlFor="time">Time:</label>
+          <input type="time" id="time" />
+        </div>
+        <div className="form-row">
+          <label htmlFor="cinema">Cinema:</label>
+          <input type="text" id="cinema" />
+        </div>
+        <div className="form-row">
+          <label htmlFor="quantity">Quantity:</label>
+          <input type="number" id="quantity" min="1" />
+        </div>
+        <div className="form-row">
+          <label htmlFor="ticket">Ticket:</label>
+          <select id="ticket">
+            <option value="standard">Standard</option>
+            <option value="premium">Premium</option>
+            <option value="vip">VIP</option>
+          </select>
+        </div>
+        <button onClick={handleReturn}>Previous Page</button>
+      </div>
+    </div>
+  );
+};
+
+export default TicketPage;
