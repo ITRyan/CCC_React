692d9411ca8d384958d716c29457c234f5568fe2
unknown <itryanchan@gmail.com>
2023-05-31 11:14:52 -0700

    prisma

--- ccc-project/src/Pages/ProfilePage.jsx
+++ ccc-project/src/Pages/ProfilePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import Sidebar from "../Components/Sidebar";
 import Input from "../Components/Input";
 import "../App.css";
@@ -6,19 +6,31 @@ const ProfilePage = () => {
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
+  const [variant, setVariant] = useState("login");
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) =>
+      currentVariant === "login" ? "register" : "login"
+    );
+  }, []);
   return (
     <>
       <div className="flex">
         <Sidebar />
         <div className="login">
-          <h2 className="Signin">Sign in </h2>
+          <h2 className="Signin">
+            {variant === "login" ? "Sign in" : "Register"}{" "}
+          </h2>
           <div className="Input">
-            <Input
-              label="Username"
-              onChange={(e) => setName(e.target.value)}
-              id="name"
-              value={name}
-            />
+            
+              {variant === "register" && (
+                <Input
+                  label="Username"
+                  onChange={(e) => setName(e.target.value)}
+                  id="name"
+                  value={name}
+                />
+              )}
+              
             <Input
               label="Email"
               onChange={(e) => setEmail(e.target.value)}
@@ -34,8 +46,20 @@ const ProfilePage = () => {
               value={password}
             />
           </div>
-          <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">Login</button>
-          <p className="text-neutral-500 mt-12">Frist time using?<span className="text-white ml-1 hover:underline cursor-pointer">Create and account</span></p>
+          <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+            {variant==="login"?"Login":"Sign up"}
+          </button>
+          <p className="text-neutral-500 mt-12">
+          {variant === "login"
+                ? "First time using Netflix?"
+                : "Already have an account?"}
+            <span
+              onClick={toggleVariant}
+              className="text-white ml-1 hover:underline cursor-pointer"
+            >
+              {variant === "login" ? "Create an account" : "Login"}
+            </span>
+          </p>
         </div>
       </div>
     </>
